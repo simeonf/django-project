3c5c52427616de84b95ca10f709cd9a619318330
Simeon Franklin <simeonf@gmail.com>
2012-12-10 17:37:04 -0800

    Completed step 4 of the tutorial

---
+++ mysite/polls/models.py
@@ -0,0 +1,30 @@
+import datetime
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Poll(models.Model):
+    user = models.ForeignKey(User)
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+    
+    def __unicode__(self):
+        return self.question
+
+    def was_published_recently(self):
+        return self.pub_date.date() == datetime.date.today()
+    
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+    
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
+
+    def __unicode__(self):
+        return self.choice
